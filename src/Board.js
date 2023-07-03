24fc51590ae8a5744e1fb007512de5f9e03d4e63
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 16:50:04 +0000

    Jul 3, 2023, 7:50 PM

--- src/Board.js
+++ src/Board.js
@@ -1,12 +1,13 @@
 import BoardSquare from './BoardSquare';
+import SquareGroup from './SquareGroup';
 
 const Board = () => {
-    const squares = Array(9).fill(null); 
+    const squares = Array(3).fill(null); 
 
     return ( 
         <div className="board" style={{display: 'flex', flexWrap: 'wrap', width: '450px'}}>
             {squares.map((value, i) =>
-                squares.map((value, j) => <BoardSquare key={i * 9 + j} />)
+                squares.map((value, j) => <SquareGroup key={i * 3 + j} />)
             )}
         </div>
     );
