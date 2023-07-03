010a92a5943507a769bbaef9ded07b028aba5fee
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 16:40:24 +0000

    Jul 3, 2023, 7:40 PM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import BoardSquare from './BoardSquare';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Navbar />
       <div className="content">
         <Home />
+        <BoardSquare/>
       </div>
     </div>
   );
