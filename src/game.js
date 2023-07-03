de1d7e1140c3baeb465651aa32fec512612b5bd7
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 23:34:51 +0000

    Jul 4, 2023, 2:34 AM

--- src/game.js
+++ src/game.js
@@ -55,12 +55,12 @@ function solve(grid) {
 
 function createPuzzle(){
     let puzzle = [];
-    for(let i = 0; i<9;i++){
+    for(let i = 0; i < 9;i++){
         puzzle[i] = Array(9).fill(0);
     }
     for(let i = 0; i < 9; i++) {
         for(let j = 0; j < 9; i++) {
-            
+            if(Math.random)
         }
     }
     return puzzle;
