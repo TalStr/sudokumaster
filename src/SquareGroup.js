5720d2f117b333bba324c295e5f631c400bd132d
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 16:51:24 +0000

    Jul 3, 2023, 7:51 PM

--- src/SquareGroup.js
+++ src/SquareGroup.js
@@ -7,7 +7,7 @@ const SquareGroup = () => {
             display: 'flex',
             flexWrap: 'wrap',
             width: '150px',
-            border: '1px solid black'
+            border: '3px solid black'
             }}>
             {squares.map((value, i) =>
                 squares.map((value, j) => <BoardSquare key={i * 3 + j} />)
