07e68e23f4c412e3765bb030e69f2fe68874a39a
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 16:33:06 +0000

    Jul 3, 2023, 7:33 PM

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+
 const Home = () => {
-  const handleClick = (name, e) =>{
-    console.log("hello " + name, e);
+
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(30);
+  const handleClick = (new_name, e) =>{
+    setName(new_name);
+    setAge(18);
   }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{ name } age is { age }</p>
       <button onClick={(e) => handleClick("Tal", e)}>Click me</button>
     </div>
   );
