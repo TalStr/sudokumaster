27115b8a92924e7e5f78859b24c7113d6f2bc47b
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 15:34:36 +0000

    Jul 3, 2023, 6:34 PM

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,6 @@ const Navbar = () => {
         <div className="links">
           <a href="/">Home</a>
           <a href="/create" style={{ 
-            color: 'white', 
             backgroundColor: '#f1356d',
             borderRadius: '8px' 
           }}>New Blog</a>
