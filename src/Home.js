3552093dfc1155258384bd2206628fa1d59572d5
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 16:22:16 +0000

    Jul 3, 2023, 7:22 PM

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 const Home = () => {
-  const handleClick = () =>{
-    console.log("hello");
+  const handleClick = (name) =>{
+    console.log("hello " + name, e);
   }
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClick("Tal")}>Click me</button>
     </div>
   );
 }
