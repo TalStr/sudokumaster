7b5575b52d4d74846dd37ae7d3eb31f68c40b535
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 16:59:26 +0000

    Jul 3, 2023, 7:59 PM

--- src/Board.js
+++ src/Board.js
@@ -1,46 +1,18 @@
-import BoardSquare from './BoardSquare';
-import SquareGroup from './SquareGroup';
+import React from 'react';
+import BoardSquare from './BoardSquare'; 
 
 const Board = () => {
-    const squares = Array(3).fill(null); 
+    const squares = Array(9).fill(null); 
 
-    return (           
-          <div className='board'>
-            <div className="parentCube">
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-            </div>
-            <div className="parentCube">
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-            </div>
-            <div className="parentCube">
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-              <div className="childCube"></div>
-            </div>
-          </div>
-              );
+    return (
+        <div className='sudoku'>
+            {squares.map((_, i) => (
+                <div key={i} className="parentCube">
+                    {squares.map((_, j) => <BoardSquare key={i * 9 + j} />)}
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default Board;
