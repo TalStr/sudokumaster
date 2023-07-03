71d510fb8fa2d006295d23a8737f288a15f723a2
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 16:41:30 +0000

    Jul 3, 2023, 7:41 PM

--- src/BoardSquare.js
+++ src/BoardSquare.js
@@ -1,6 +1,10 @@
 const BoardSquare = () => {
     return ( 
-        <div className="boardsquare">
+        <div className="boardsquare" style={{
+            width: "200px",
+            height: "200px",
+            border: "1px solid black"
+        }}>
         </div>
      );
 }
