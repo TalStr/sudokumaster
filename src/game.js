70c48ffd967729b547b924609d4ca755e5be390d
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 23:34:30 +0000

    Jul 4, 2023, 2:34 AM

--- src/game.js
+++ src/game.js
@@ -53,5 +53,17 @@ function solve(grid) {
     return true
 }
 
+function createPuzzle(){
+    let puzzle = [];
+    for(let i = 0; i<9;i++){
+        puzzle[i] = Array(9).fill(0);
+    }
+    for(let i = 0; i < 9; i++) {
+        for(let j = 0; j < 9; i++) {
+            
+        }
+    }
+    return puzzle;
+}
 solve(board);
 console.log(board);
