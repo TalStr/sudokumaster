62ee6a4fdac797b89327095d14095342586c98e0
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 16:49:37 +0000

    Jul 3, 2023, 7:49 PM

--- src/SquareGroup.js
+++ src/SquareGroup.js
@@ -1,7 +1,12 @@
 const SquareGroup = () => {
     const squares = Array(3).fill(null); 
     return ( 
-        <div className="squaregroup" style={{display: 'flex', flexWrap: 'wrap', width: '150px'}}>
+        <div className="squaregroup" style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            width: '150px',
+            border: '1px solid black'
+            }}>
             {squares.map((value, i) =>
                 squares.map((value, j) => <BoardSquare key={i * 3 + j} />)
             )}
