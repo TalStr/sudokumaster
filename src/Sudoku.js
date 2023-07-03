5bb84a04717aefcf5ce115df5bf5c807652f840f
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 23:15:01 +0000

    Jul 4, 2023, 2:15 AM

--- src/Sudoku.js
+++ src/Sudoku.js
@@ -2,12 +2,20 @@ import React, { useState, useRef} from "react";
 import Board from "./ui/Board"
 import Interface from "./ui/Interface"
 
+function getGrid() {
+    let grid = [];
+    for (let i = 0; i<9; i++){
+        grid.push(Array(9).fill(0));
+    }
+    return grid;
+}
+
 function Sudoku() {
-    const [grid, setGrid] = useState()
+    const [grid, setGrid] = useState(getGrid)
     return (
         <div className="Sudoku">
             <Board puzzle={puzzle} grid={grid}/>
             <Interface/>
         </div>
-    )
+    );
 }
