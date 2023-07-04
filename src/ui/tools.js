e440e7b92a13cd56899100d0feecbe18846ff49f
BuildTools <unconfigured@null.spigotmc.org>
2023-07-04 01:17:54 +0000

    Jul 4, 2023, 4:17 AM

--- src/ui/tools.js
+++ src/ui/tools.js
@@ -1,9 +1,9 @@
 const Tools = () => {
     return ( 
         <div className="tools">
-            <button><img src="und" alt="icon1" /></button>
-            <button><img src="/path/to/your/image2.png" alt="icon2" /></button>
-            <button><img src="/path/to/your/image3.png" alt="icon3" /></button>
+            <button><img src="../images/undo-arrow.png" alt="icon1" /></button>
+            <button><img src="../images/undo-arrow.png" alt="icon2" /></button>
+            <button><img src="../images/undo-arrow.png" alt="icon3" /></button>
 
         </div>
      );
