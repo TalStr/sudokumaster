4121a99844c6fcc7998940f85774cee64d1ab995
BuildTools <unconfigured@null.spigotmc.org>
2023-07-04 00:59:09 +0000

    Jul 4, 2023, 3:59 AM

--- src/ui/Counter.js
+++ src/ui/Counter.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 function Counter(){
     const [count, setCount] = useState(0);
     return (
-        <div className="mistake-counter">count</div>
+        <div className="mistake-counter">{count}</div>
     );
 } 
 export default Counter;
