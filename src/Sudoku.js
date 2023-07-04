4e78421b1e49c2a949e45fd0e4fc312cb956f498
BuildTools <unconfigured@null.spigotmc.org>
2023-07-04 00:14:10 +0000

    Jul 4, 2023, 3:14 AM

--- src/Sudoku.js
+++ src/Sudoku.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef} from "react";
 import Board from "./ui/Board"
 import Interface from "./ui/Interface"
-
+import './App.css';
 
 function getGrid() {
     let grid = [];
