479e4635005139a70bad85af63d7bcd0cddd8cdd
BuildTools <unconfigured@null.spigotmc.org>
2023-07-04 00:46:55 +0000

    Jul 4, 2023, 3:46 AM

--- src/ui/Board.js
+++ src/ui/Board.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Tile from './Tile'
-function Board({ puzzle, grid, handleTileClick }){
+function Board({ puzzle, grid, onTileClick }){
     return (
 	<div className="board">
-		<Tile puzzle={puzzle} grid={grid} onTileClick={handleTileClick}/>
+		<Tile puzzle={puzzle} grid={grid} onTileClick={onTileClick}/>
 	</div>    
 	);
 }
