75d6460334ba8a40085b6865fa9132c995b7312c
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 00:44:14 +0000

    Jul 10, 2023, 3:44 AM

--- src/ui/Board.js
+++ src/ui/Board.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tile from './Tile'
 function Board({ puzzle, grid, onTileClick }){
+	const [selectedTile, setSelectedTile] = useState({ row: 0, col: 0 });
     return (
 	<div className="board">
 		<Tile puzzle={puzzle} grid={grid} onTileClick={onTileClick}/>
