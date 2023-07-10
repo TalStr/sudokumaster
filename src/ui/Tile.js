39b41b6ca8e7aa92b13464dc3a5a3cb9e0783ee9
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 02:28:29 +0000

    Jul 10, 2023, 5:28 AM

--- src/ui/Tile.js
+++ src/ui/Tile.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 function Tile({ value, isInitial, isSelected, onClick, selectedValue, selectedTile, row, col }) {
     const isSameRowOrCol = selectedTile && (selectedTile.row === row || selectedTile.col === col);
-
     return (
         <div 
             className={
                 "tile" +
                 (isSelected ? " selected" : "") +
                 (isInitial ? " initial" : "") +
-                (value !== 0 && value === selectedValue ? " same-value" : "") +
+                (value !== 0 && !isSelected && value === selectedValue ? " same-value" : "") +
                 (value !== 0 ? " taken" : "") +
                 (isSameRowOrCol && !isSelected ? " same-row-col" : "")
             }
