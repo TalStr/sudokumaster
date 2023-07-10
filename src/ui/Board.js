9016fe385abd26d261db2bd2edb83be8a39c5a03
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 02:01:04 +0000

    Jul 10, 2023, 5:01 AM

--- src/ui/Board.js
+++ src/ui/Board.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import Tile from './Tile'
 
-function Board({ puzzle, grid }){
+function Board({ puzzle, grid, setSelectedTile, setSelectedValue }){
     const [selectedTile, setSelectedTile] = useState({ row: 0, col: 0 });
 
-    const handleTileClick = (row, col) => {
-        setSelectedTile({ row, col, value: grid[row][col] });
-    };
-
+	const handleTileClick = (row, col) => {
+		setSelectedTile({ row, col, value: grid[row][col] });
+		setSelectedValue(grid[row][col]);
+	  };
+	
     return (
         <div className="board">
             {grid.map((row, rowIndex) => {
