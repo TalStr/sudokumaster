cbb91d1e31f9fd147eaf50c59b56d29b989ee989
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 07:47:31 +0000

    Jul 10, 2023, 10:47 AM

--- src/ui/WinDialog.jsx
+++ src/ui/WinDialog.jsx
@@ -5,6 +5,7 @@ export const WinDialog = ({ openDialog, setOpenDialog, time }) => {
           <div className="win-dialog">
             <div className="dialog-container">
               <h3>You Win</h3>
+              <button>next</button>
             </div>
           </div>
         )}
