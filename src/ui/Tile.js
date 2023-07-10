811332335dedbcd2c2083fe3433087b8f2aa8caa
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 02:49:54 +0000

    Jul 10, 2023, 5:49 AM

--- src/ui/Tile.js
+++ src/ui/Tile.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Tile({ value, isInitial, isSelected, onClick, selectedValue, selectedTile, row, col }) {
     const isSameRowOrCol = selectedTile && (selectedTile.row === row || selectedTile.col === col);
-    const borderStyle = {
-        border-bottom: (row == 8) ? '4px' : '2px',
-        borderRightWidth: (col === 8) ? '4px' : '2px',
-        borderLeftWidth: (col % 3 == 0) ? '4px' : '2px',
-        borderTopWidth: (row % 3 == 0) ? '4px' : '2px',
+    // const borderStyle = {
+    //     border-bottom: (row == 8) ? '4px' : '2px',
+    //     borderRightWidth: (col === 8) ? '4px' : '2px',
+    //     borderLeftWidth: (col % 3 == 0) ? '4px' : '2px',
+    //     borderTopWidth: (row % 3 == 0) ? '4px' : '2px',
     
-    }
+    // }
     return (
         <div 
             className={
@@ -20,7 +20,11 @@ function Tile({ value, isInitial, isSelected, onClick, selectedValue, selectedTi
                 (isSameRowOrCol && !isSelected ? " same-row-col" : "")
             }
             style={
-                
+                borderBottomWidth: (row == 8) ? '4px' : '2px',
+                borderRightWidth: (col === 8) ? '4px' : '2px',
+                borderLeftWidth: (col % 3 == 0) ? '4px' : '2px',
+                borderTopWidth: (row % 3 == 0) ? '4px' : '2px',
+
             }
             onClick={onClick}
         >
