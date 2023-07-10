266ffec0e4efce3233cab6538e2a979dc3b14329
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 06:56:23 +0000

    Jul 10, 2023, 9:56 AM

--- src/ui/Timer.js
+++ src/ui/Timer.js
@@ -1,20 +1,21 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Timer = ({ seconds, setSeconds}) => {
-    useEffect(() => {
-        const timer = setInterval(() => {
-          setSeconds((prevSeconds) => prevSeconds + 1);
-        }, 1000);
-    
-        return () => {
-          clearInterval(timer);
-        };
-    }, [setSeconds]);
-        return(
-        <div className='timer'>
-            {seconds};
-        </div>
-    );
+const Timer = ({ seconds, setSeconds }) => {
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [setSeconds]);
+
+  return (
+    <div className='timer'>
+      {seconds}
+    </div>
+  );
 }
 
 export default Timer;
