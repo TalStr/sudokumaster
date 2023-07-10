80e19deffef4d8f1fe10d52e9ba7c5a25afe5837
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 03:46:29 +0000

    Jul 10, 2023, 6:46 AM

--- src/ui/Interface.js
+++ src/ui/Interface.js
@@ -1,11 +1,17 @@
-function Interface({ onButtonClick, setSelectedValue }) {
+function Interface({ onButtonClick, setSelectedValue, counters }) {
     return ( 
         <div className="interface">
             {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
-                <button key={number} onClick={() => {
-                  setSelectedValue(number); 
-                  onButtonClick(number);
-                }}>
+                <button 
+                    key={number} 
+                    onClick={() => {
+                        setSelectedValue(number); 
+                        onButtonClick(number);
+                    }}
+                    style= {{
+                        
+                    }}
+                >
                     {number}
                 </button>
             ))}
