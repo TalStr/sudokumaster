c823cd28561dbdd1af29fcd5105b5936c4ef218e
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 02:03:46 +0000

    Jul 10, 2023, 5:03 AM

--- src/ui/Tile.js
+++ src/ui/Tile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Tile({ value, isInitial, isSelected, onClick, selectedValue, selectedTile }) {
+function Tile({ value, isInitial, isSelected, onClick, selectedValue, selectedTile, row, col }) {
     let className = "tile";
     if(isSelected) className += " selected";
     else if(isInitial) className += " initial";
