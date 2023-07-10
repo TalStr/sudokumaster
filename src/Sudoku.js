2c4d355c0fb75c0088ed5cab84e4fdbe08258e62
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 03:27:50 +0000

    Jul 10, 2023, 6:27 AM

--- src/Sudoku.js
+++ src/Sudoku.js
@@ -84,6 +84,7 @@ function getRandomSudoku(){
 }
 
 function isComplete(grid) {
+    console.log(grid.map(row => row.join(' ')).join('\n'));
     for (let i = 0; i < grid.length; i++) {
       for (let j = 0; j < grid[i].length; j++) {
         if (grid[i][j] === 0) {
